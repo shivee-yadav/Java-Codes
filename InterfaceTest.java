3649594e503d954770b5b42f62f8d6b28c0e7d92
shivee yadav <shiveeyadav089@gmail.com>
2023-05-06 23:50:07 +0530

    new files

---
+++ InterfaceTest.java
@@ -0,0 +1,25 @@
+class Circle implements Area{
+	public float compute(float a, float b) {
+		return pi*a*a;
+	}
+	
+}
+
+
+class Rectangle implements Area{
+	public float compute(float a,float b) {
+		return a*b;
+	}
+}
+
+
+public class InterfaceTest {
+	public static void main(String args[]) {
+		Circle c=new Circle();
+		Rectangle r= new Rectangle();
+		System.out.println("Area of Circle = "+c.compute(10, 0));
+		System.out.println("Area of Rectangle="+r.compute(10, 20));
+		
+	}
+
+}
