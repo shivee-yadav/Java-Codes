272da4b74ba540083bc5f5908d3140ebd1d28b43
shivee yadav <shiveeyadav089@gmail.com>
2023-10-04 23:55:40 +0530

    Update KeyInput.java

--- KeyInput.java
+++ KeyInput.java
@@ -4,7 +4,7 @@ public class KeyInput {
 	public static void main(String[] args) throws IOException{
 		BufferedReader in=new BufferedReader(new InputStreamReader(System.in));//bufferedreader-->memory
 		String name=in.readLine();
-		System.out.println("Your name is :"+name);
+		System.out.println("Your name is  :"+name);
 		//system.in->reads from keyboard
 		//system.file->reads from file
 
